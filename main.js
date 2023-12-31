1fed027622b337830e4ae5f89a1cf62f0e48a677
Katelli <katrine.ask96@gmail.com>
2023-12-31 23:46:47 +0100

    Added animation

--- main.js
+++ main.js
@@ -20,5 +20,9 @@ let x = setInterval(function(){
         clearInterval(x)
         document.querySelector("#timer").textContent = "Happy New Year!";
         document.querySelector("#countdown").style.display = "none"
+        document.querySelector("#fireworks1").style.display = "none"
+        document.querySelector("#fireworks2").style.display = "none"
+        document.querySelector("#fireworks3").style.display = "none"
+        document.querySelector("#fireworks4").style.display = "none"
     }    
 }, 1000)
